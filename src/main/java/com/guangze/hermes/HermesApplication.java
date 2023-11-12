f7d1cb8bdcfbad438d4dd54b8c40d15254f5f770
daiguangze <daiguangze.z@qq.com>
2023-11-12 21:50:25 +0800

    初始化测试

---
+++ src/main/java/com/guangze/hermes/HermesApplication.java
@@ -0,0 +1,16 @@
+package com.guangze.hermes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HermesApplication {
+    private Logger logger = LoggerFactory.getLogger(HermesApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesApplication.class, args);
+    }
+}
---
+++ src/main/java/com/guangze/hermes/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.guangze.hermes.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
+
+    @RequestMapping("/verify")
+    public ResponseEntity<String> verify(String token){
+        logger.info("token:{}",token);
+        // token验证
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @RequestMapping("/healthz")
+    public String healthz(){
+        return "ok";
+    }
+}
